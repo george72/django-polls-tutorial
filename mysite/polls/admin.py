4d6f4767dcf345399c5abfc83b679aea527a5d09
Jorge Cardenas <jorgecardenas1@hotmail.com>
2023-07-20 20:09:42 -0500

    Inicial Tutorial Polls en Django 4.x hasta antes de createsuperuser

---
+++ mysite/polls/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+
+from .models import Question, Choice #, Inventario
+
+admin.site.register(Question)
+
+admin.site.register(Choice)
+
+# admin.site.register(Inventario)
